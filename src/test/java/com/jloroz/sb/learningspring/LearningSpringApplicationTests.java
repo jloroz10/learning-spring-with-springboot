4d5f6435dd020d9a22bf76e10eaffb3973b72c60
Luis Orozco <jlorozco.1025@gmail.com>
2022-02-04 14:46:47 -0600

    first commit adding a static html to test

---
+++ src/test/java/com/jloroz/sb/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jloroz.sb.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
