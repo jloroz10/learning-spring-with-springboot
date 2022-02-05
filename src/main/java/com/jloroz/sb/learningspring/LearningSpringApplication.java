a4c8c2d9feea5d8466436c57033644c79b437680
Luis Orozco <jlorozco.1025@gmail.com>
2022-02-04 19:54:04 -0600

    creating the roomReservation Controller

--- src/main/java/com/jloroz/sb/learningspring/LearningSpringApplication.java
+++ src/main/java/com/jloroz/sb/learningspring/LearningSpringApplication.java
@@ -20,41 +20,4 @@ public class LearningSpringApplication {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
 
-	@RestController
-	@RequestMapping("/rooms")
-	public class RoomController{
-
-		@Autowired
-		private RoomRepository roomRepository;
-
-		@GetMapping
-		public Iterable<Room> getRooms() {
-			return roomRepository.findAll();
-
-		}
-	}
-	@RestController
-	@RequestMapping("/guests")
-	public class GuestController{
-		@Autowired
-		private GuestRepository guestRepository;
-
-		@GetMapping
-		public Iterable<Guest> getGuests(){
-			return guestRepository.findAll();
-		}
-	}
-
-	@RestController
-	@RequestMapping("/reservations")
-	public class ReservationController{
-		@Autowired
-		private ReservationRepository reservationRepository;
-
-		@GetMapping
-		public Iterable<Reservation> getGuests(){
-			return reservationRepository.findAll();
-		}
-	}
-
 }
---
+++ src/main/java/com/jloroz/sb/learningspring/web/RoomReservationWebController.java
@@ -0,0 +1,29 @@
+package com.jloroz.sb.learningspring.web;
+
+import com.jloroz.sb.learningspring.business.domain.RoomReservation;
+import com.jloroz.sb.learningspring.business.service.ReservationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationWebController {
+
+    private ReservationService reservationService;
+
+    @GetMapping
+    public String getReservations(@RequestParam(value="date",required = false)String dateString, Model model){
+        Date date = DateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservationList =  reservationService.getRoomReservationsForDate(date);
+
+        model.addAttribute("roomReservations",roomReservationList);
+
+        return "reservations";
+    }
+}
